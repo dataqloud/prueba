d2a1ff3ea1eaaaefc109f0212efca0e36be1add1
dataqloud <90100925+dataqloud@users.noreply.github.com>
2021-09-04 10:57:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+  
 from flask import Flask
 app = Flask(__name__)
-a = 5
-b = 4
-c = a + b
+
 @app.route('/')
 def hello_world():
-    return c
+    a = 9
+    b = 5
+    c = a + b
+    return 'Hello, World!'
